1be60c7c23da4eb62cb470d9ffbf4b640be7a72f
Charlie <charlie@closenavigator.com>
2025-03-25 20:17:12 -0600

    fix: Update webpack alias to use absolute path and add Railway config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
+import path from 'path'
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -11,7 +11,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': './src',
+      '@': path.join(process.cwd(), 'src'),
     }
     return config
   },
