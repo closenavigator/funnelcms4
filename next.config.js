c4537cda571dffe58454d036b28882afb559848f
Charlie <charlie@closenavigator.com>
2025-03-25 22:41:39 -0600

    fix: Use ES modules compatible dirname resolution

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import redirects from './redirects.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
